a62bd82281822b93d5a6374812097005fcaed9de
cheikh <ctidiane.gueye1@univ-thies.sn>
2021-06-05 18:15:03 +0000

    correction dune petite erreur au niveau de la l1

--- resources/views/pages_li1/suggestions.blade.php
+++ resources/views/pages_li1/suggestions.blade.php
@@ -5,133 +5,147 @@
     <div class="row g-5 py-5">
 
         <div class="feature col-md-4">
-        <h2>Algorithme et Programmation 1</h2>
-        <a href="#" class="icon-link" data-bs-toggle="modal" data-bs-target="#algo1suggestions">
-            Découvrir!!
-            <i class="fas fa-directions"></i>
-        </a>
-        <!-- Modal -->
-        <div class="modal fade" id="algo1suggestions" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-scrollable">
-                <div class="modal-content">
-                    <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Suggestions</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <ul>
-                            <li>
-                                <a href="img/l1/suggestions/openclassroom.pdf">
-                                    Apprendre à programmer en C(pdf)
-                                </a>
-                            </li>
-                            <li>
-                                <a href="img/suggestions/compilez-sous-gnu-linux.pdf">
-                                    Compiler sous GNU/Linux
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://youtu.be/y-i96kqT53A">
-                                    Installer MinGW
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://youtu.be/cK0Q3XHDjTY?list=PLA6zApbO92QbZ5gJaGcfgzmtuPMdJTOkQ">Notions de base en Algorithme 1</a>
-                            </li>
-                            <li>
-                                <a href="https://www.youtube.com/watch?v=7mKOoA-F3uY&list=PLA6zApbO92QbZ5gJaGcfgzmtuPMdJTOkQ&index=2">Notions de base en Algorithme 2</a>
-                            </li>
-                            <li>
-                                <a href="https://www.youtube.com/watch?v=eQD6uAO9NH4&list=PLA6zApbO92QbZ5gJaGcfgzmtuPMdJTOkQ&index=3">Introduction au langage de programmation C 1</a>
-                            </li>
-                            <li>
-                                <a href="https://www.youtube.com/watch?v=kgyqJ_oXN6M&list=PLA6zApbO92QbZ5gJaGcfgzmtuPMdJTOkQ&index=4">Introduction au langage de programmation C 2</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermé</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--fin modal-->
-
-      </div>
-
-      <div class="feature col-md-4">
-        <h2>Algorithme et Programmation 2</h2>
-        <a href="#" class="icon-link" data-bs-toggle="modal" data-bs-target="#algo2suggestions">
-            Découvrir!!
-            <i class="fas fa-directions"></i>
-        </a>
-
-        <!-- Modal -->
-        <div class="modal fade" id="algo2suggestions" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-scrollable">
-                <div class="modal-content">
-                    <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Suggestions</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <ul>
-                            <li>
-                                <a href="img/l1/suggestions/openclassroom.pdf">
-                                    Apprendre à programmer en C(pdf)
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://www.youtube.com/watch?v=eQD6uAO9NH4&list=PLA6zApbO92QbZ5gJaGcfgzmtuPMdJTOkQ&index=3">Introduction au langage de programmation C 1</a>
-                            </li>
-                            <li>
-                                <a href="https://www.youtube.com/watch?v=kgyqJ_oXN6M&list=PLA6zApbO92QbZ5gJaGcfgzmtuPMdJTOkQ&index=4">Introduction au langage de programmation C 2</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermé</button>
+            <h2>Algorithme et Programmation 1</h2>
+            <a href="#" class="icon-link" data-bs-toggle="modal" data-bs-target="#algo1suggestions">
+                Découvrir!!
+                <i class="fas fa-directions"></i>
+            </a>
+            <!-- Modal -->
+            <div class="modal fade" id="algo1suggestions" data-bs-backdrop="static" data-bs-keyboard="false"
+                tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-scrollable">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="staticBackdropLabel">Suggestions</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <ul>
+                                <li>
+                                    <a href="img/l1/suggestions/openclassroom.pdf">
+                                        Apprendre à programmer en C(pdf)
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="img/suggestions/compilez-sous-gnu-linux.pdf">
+                                        Compiler sous GNU/Linux
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://youtu.be/y-i96kqT53A">
+                                        Installer MinGW
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://youtu.be/cK0Q3XHDjTY?list=PLA6zApbO92QbZ5gJaGcfgzmtuPMdJTOkQ">Notions
+                                        de base en Algorithme 1</a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://www.youtube.com/watch?v=7mKOoA-F3uY&list=PLA6zApbO92QbZ5gJaGcfgzmtuPMdJTOkQ&index=2">Notions
+                                        de base en Algorithme 2</a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://www.youtube.com/watch?v=eQD6uAO9NH4&list=PLA6zApbO92QbZ5gJaGcfgzmtuPMdJTOkQ&index=3">Introduction
+                                        au langage de programmation C 1</a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://www.youtube.com/watch?v=kgyqJ_oXN6M&list=PLA6zApbO92QbZ5gJaGcfgzmtuPMdJTOkQ&index=4">Introduction
+                                        au langage de programmation C 2</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermé</button>
+                        </div>
                     </div>
                 </div>
             </div>
+            <!--fin modal-->
+
         </div>
-        <!--fin modal-->
 
-      </div>
+        <div class="feature col-md-4">
+            <h2>Algorithme et Programmation 2</h2>
+            <a href="#" class="icon-link" data-bs-toggle="modal" data-bs-target="#algo2suggestions">
+                Découvrir!!
+                <i class="fas fa-directions"></i>
+            </a>
 
-      <div class="feature col-md-4">
-        <h2>Systèmes d'Exploitation </h2>
-        <a href="#" class="icon-link" data-bs-toggle="modal" data-bs-target="#SEsuggestions">
-            Découvrir!!
-            <i class="fas fa-directions"></i>
-        </a>
-
-        <!-- Modal -->
-        <div class="modal fade" id="SEsuggestions" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-scrollable">
-                <div class="modal-content">
-                    <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Suggestions</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <ul>
-                            <li>
-                                <a href="img/suggestions/reprenez-le-controler-a-l-aide-de-linux.pdf">
-                                    Reprenez le controle à l'aide de Linux!
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermé</button>
+            <!-- Modal -->
+            <div class="modal fade" id="algo2suggestions" data-bs-backdrop="static" data-bs-keyboard="false"
+                tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-scrollable">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="staticBackdropLabel">Suggestions</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <ul>
+                                <li>
+                                    <a href="img/l1/suggestions/openclassroom.pdf">
+                                        Apprendre à programmer en C(pdf)
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://www.youtube.com/watch?v=eQD6uAO9NH4&list=PLA6zApbO92QbZ5gJaGcfgzmtuPMdJTOkQ&index=3">Introduction
+                                        au langage de programmation C 1</a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="https://www.youtube.com/watch?v=kgyqJ_oXN6M&list=PLA6zApbO92QbZ5gJaGcfgzmtuPMdJTOkQ&index=4">Introduction
+                                        au langage de programmation C 2</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermé</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <!--fin modal-->
+            <!--fin modal-->
 
-      </div>
+        </div>
+
+        <div class="feature col-md-4">
+            <h2>Systèmes d'Exploitation </h2>
+            <a href="#" class="icon-link" data-bs-toggle="modal" data-bs-target="#SEsuggestions">
+                Découvrir!!
+                <i class="fas fa-directions"></i>
+            </a>
+
+            <!-- Modal -->
+            <div class="modal fade" id="SEsuggestions" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-scrollable">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="staticBackdropLabel">Suggestions</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <ul>
+                                <li>
+                                    <a href="img/suggestions/reprenez-le-controle-a-l-aide-de-linux.pdf">
+                                        Reprenez le controle à l'aide de Linux!
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermé</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--fin modal-->
+
+        </div>
 
     </div>
 </div>
