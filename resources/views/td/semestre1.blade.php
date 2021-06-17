1b392ec022154df6958fb58645ec771dd843748e
cheikh <ctidiane.gueye1@univ-thies.sn>
2021-06-17 18:29:33 +0000

    ajout de certains td

--- resources/views/td/semestre1.blade.php
+++ resources/views/td/semestre1.blade.php
@@ -1,13 +1,8 @@
 @extends('template')
 @section('content')
-
-
 <div class="container py-5" id="featured-3">
-
     <h2 class="pb-2 border-bottom">TD Semestre 1</h2>
-
     <div class="row g-5 py-5">
-
         <div class="feature col-md-4">
             <h2>Mathématiques</h2>
             <p>
@@ -15,18 +10,29 @@
                     <li>
                         Analyse 1
                     </li>
-
                     <li>
                         Algèbre 1
                     </li>
                 </ul>
             </p>
-            <a href="#" class="icon-link">
-                Non disponible!!
-                <i class="fas fa-directions"></i>
-            </a>
+            <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" type="button" id="mathematique1"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    Disponible
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="mathematique1">
+                    <li><a class="dropdown-item"
+                            href="https://drive.google.com/file/d/1UDyNYqW_mj9QcM3F2b8J2MAj7n9tyWS7/view?usp=sharing">Examen
+                            1</a></li>
+                    <li> <a class="dropdown-item"
+                            href="https://drive.google.com/file/d/1B5Sc7xN8ecuLTeTDNRJEWCZOYsD2bkS5/view?usp=sharing">Examen
+                            2</a></li>
+                    <li> <a class="dropdown-item"
+                            href="https://drive.google.com/file/d/1tQAtJG6dH1cd7OWLxclaBfQVGteJB3G3/view?usp=sharing">Examen
+                            3</a></li>
+                </ul>
+            </div>
         </div>
-
         <div class="feature col-md-4">
             <h2>Physique</h2>
             <p>
@@ -39,11 +45,56 @@
                     </li>
                 </ul>
             </p>
-            <a href="#" class="icon-link">
-                Non disponible!!
-            </a>
+            <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" type="button" id="fondamentaux"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    Fondamentaux
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="fondamentaux">
+                    <h5 class="border-bottom">Fondemantaux</h5>
+                    <li><a class="dropdown-item"
+                            href="https://drive.google.com/file/d/1gMKyIMeNkDxUU4W8SCKKHDPtQE7seYOd/view?usp=sharing">TD</a>
+                    </li>
+                    <li> <a class="dropdown-item"
+                            href="https://drive.google.com/file/d/17YRWUPYBnzomx0lMkpPPShQM3oAifBkg/view?usp=sharing">
+                            Correction TD 1</a></li>
+                    <li> <a class="dropdown-item"
+                            href="https://drive.google.com/file/d/1FM1PZOie8aqQDLvOmCYCxkaaD2sQkkrN/view?usp=sharing">
+                            Correction TD 2</a></li>
+                    <li> <a class="dropdown-item"
+                            href="https://drive.google.com/file/d/1M11qxpxZ0VomO2b5wBZM3KBTJyuITD3M/view?usp=sharing">
+                            Examen 1</a></li>
+                    <li> <a class="dropdown-item"
+                            href="https://drive.google.com/file/d/1hgStuFFfYnoE3y8PxMh7Lcix0lZexUW0/view?usp=sharing">
+                            Examen 2</a></li>
+                    <li> <a class="dropdown-item"
+                            href="https://drive.google.com/file/d/1Ocx4flhboghr29-jt6ECdnX2vNQa46hQ/view?usp=sharing">
+                            Examen 3</a></li>
+                </ul>
+                <button class="btn btn-primary dropdown-toggle" type="button" id="electricite" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Electricité
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="electricite">
+                    <h5 class="border-bottom">Electricite</h5>
+                    <li><a class="dropdown-item"
+                            href="https://drive.google.com/file/d/1gV6bmpMI3Sn7CnpiW2DbJwr9JwB_8Jig/view?usp=sharing">Sujet
+                            Devoir</a></li>
+                    <li> <a class="dropdown-item"
+                            href="https://drive.google.com/file/d/103fzv5fF3Ljg1XxCl7_g2bz3iH-nS2gv/view?usp=sharing">Examen
+                            Electricite 1</a></li>
+                    <li> <a class="dropdown-item"
+                            href="https://drive.google.com/file/d/1sUCQ86SXn0xp_TCOUCmo9_Ub6o9-SGe5/view?usp=sharing">Examen
+                            d'Electricite 2</a></li>
+                    <li> <a class="dropdown-item"
+                            href="https://drive.google.com/file/d/1ccv2c_PGoTS9F4TXvZk_6SCjMxakRBJb/view?usp=sharing">Examen
+                            d'Electricite 3</a></li>
+                    <li> <a class="dropdown-item"
+                            href="https://drive.google.com/file/d/1TL52lRN-L8oukzJcsBqlDvUKqVVDGXrm/view?usp=sharing">Epreuve
+                            d'Electricite 4</a></li>
+                </ul>
+            </div>
         </div>
-
         <div class="feature col-md-4">
             <h2>Informatique</h2>
             <p>
@@ -56,11 +107,9 @@
                     </li>
                 </ul>
             </p>
-            <a href="#" class="icon-link" data-bs-toggle="modal" data-bs-target="#tdInformatique1">
-                S'exercer!!
-            </a>
-
-
+            <button href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tdInformatique1">
+                Disponible
+            </button>
             <!-- Modal -->
             <div class="modal fade" id="tdInformatique1" data-bs-backdrop="static" data-bs-keyboard="false"
                 tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -76,28 +125,67 @@
                                     Algorithmique et programmation 1
                                     <ul>
                                         <li>
-                                            <a
-                                                href="https://drive.google.com/file/d/10hGLkmTg6Sp0ZYsO6-WEXiR_ll6ObZDV/view?usp=sharing">Generalites
-                                                et Instruction de Base</a> &nbsp;
-                                            <a href="">
-                                                Correction
-                                            </a>
+                                            <ol>
+                                                <li>
+                                                    <a
+                                                        href="https://drive.google.com/file/d/10hGLkmTg6Sp0ZYsO6-WEXiR_ll6ObZDV/view?usp=sharing">Generalites
+                                                        et Instruction de Base</a> &nbsp;
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        href="https://github.com/cheikh0101/correctionAlgo1/blob/main/fiche1/exo1.c">
+                                                        Proposition de Correction
+                                                    </a>
+                                                </li>
+                                            </ol>
                                         </li>
+                                        <hr>
                                         <li>
-                                            <a
-                                                href="https://drive.google.com/file/d/1EvKR5i0ZAtwqLFsyLCT6MMETsB8c08Xw/view?usp=sharing">Structures
-                                                et Opérations de Base</a>
+                                            <ol>
+                                                <li>
+                                                    <a
+                                                        href="https://drive.google.com/file/d/1EvKR5i0ZAtwqLFsyLCT6MMETsB8c08Xw/view?usp=sharing">Structures
+                                                        et Opérations de Base</a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        href="https://github.com/cheikh0101/correctionAlgo1/tree/main/fiche2">
+                                                        Proposition de Correction
+                                                    </a>
+                                                </li>
+                                            </ol>
                                         </li>
+                                        <hr>
                                         <li>
-                                            <a
-                                                href="https://drive.google.com/file/d/1aStGhEUz4cajb67SCEKruO13U9pLBBfX/view?usp=sharing">Structures
-                                                de Controle</a>
+                                            <ol>
+                                                <li>
+                                                    <a
+                                                        href="https://drive.google.com/file/d/1aStGhEUz4cajb67SCEKruO13U9pLBBfX/view?usp=sharing">Structures
+                                                        de Controle</a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        href="https://github.com/cheikh0101/correctionAlgo1/tree/main/fiche3">Proposition
+                                                        de Correction</a>
+                                                </li>
+                                            </ol>
+
                                         </li>
+                                        <hr>
                                         <li>
-                                            <a
-                                                href="https://drive.google.com/file/d/1W2JFLCGBLoCTjBXOVfeF4WsaYsGkJ38x/view?usp=sharing">Structures
-                                                Selectives</a>
+                                            <ol>
+                                                <li>
+                                                    <a
+                                                        href="https://drive.google.com/file/d/1W2JFLCGBLoCTjBXOVfeF4WsaYsGkJ38x/view?usp=sharing">Structures
+                                                        Selectives</a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        href="https://github.com/cheikh0101/correctionAlgo1/tree/main/fiche4">Correction</a>
+                                                </li>
+                                            </ol>
                                         </li>
+                                        <hr>
                                         <li>
                                             <a
                                                 href="https://drive.google.com/file/d/1a4rIfYCcmxSIaF_tX-yLvwrnPXqxj3xm/view?usp=sharing">Examen
@@ -109,7 +197,8 @@
                                     Introduction aux systèmes d’exploitation
                                     <ul>
                                         <li>
-                                            <a href=""></a>
+                                            <a
+                                                href="https://drive.google.com/file/d/1uA91J3ujMbfON7TJ3ErAe5FuzcogBdZj/view?usp=sharing">Examen</a>
                                         </li>
                                     </ul>
                                 </li>
--- resources/views/td/semestre2.blade.php
+++ resources/views/td/semestre2.blade.php
@@ -1,14 +1,9 @@
 @extends('template')
 
 @section('content')
-
-
 <div class="container py-5" id="featured-3">
-
     <h2 class="pb-2 border-bottom">TD Semestre 2</h2>
-
     <div class="row g-5 py-5">
-
         <div class="feature col-md-4">
             <h2>Mathématiques</h2>
             <p>
@@ -22,29 +17,51 @@
                     </li>
                 </ul>
             </p>
-            <a href="#" class="icon-link">
-                Non disponible!!
-                <i class="fas fa-directions"></i>
-            </a>
+            <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" type="button" id="mathematique2"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    Disponible
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="mathematique2">
+                    <li><a class="dropdown-item"
+                            href="https://drive.google.com/file/d/1UDyNYqW_mj9QcM3F2b8J2MAj7n9tyWS7/view?usp=sharing">Examen
+                            1</a></li>
+                    <li> <a class="dropdown-item"
+                            href="https://drive.google.com/file/d/1B5Sc7xN8ecuLTeTDNRJEWCZOYsD2bkS5/view?usp=sharing">Examen
+                            2</a></li>
+                    <li> <a class="dropdown-item"
+                            href="https://drive.google.com/file/d/1tQAtJG6dH1cd7OWLxclaBfQVGteJB3G3/view?usp=sharing">Examen
+                            3</a></li>
+                </ul>
+            </div>
         </div>
-
         <div class="feature col-md-4">
             <h2>Physique</h2>
             <p>
                 <ul>
                     <li>
-                        Fondamentaux de Physique
+                        Ondes et Propagation
                     </li>
                     <li>
-                        Electricité
+                        Electronique
                     </li>
                 </ul>
             </p>
-            <a href="#" class="icon-link">
-                Non disponible!!
-            </a>
+            <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" type="button" id="electronique2"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    Disponible
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="electronique2">
+                    <li><a class="dropdown-item"
+                            href="https://drive.google.com/file/d/1A15SGPx2tKpxXwHEzddztqyOABcwsA8k/view?usp=sharing">Série
+                            Diodes</a></li>
+                    <li> <a class="dropdown-item"
+                            href="https://drive.google.com/file/d/1npOQgjumZenMJoUh-JrjsVW3p3f20tLL/view?usp=sharing">TD
+                            Electronique</a></li>
+                </ul>
+            </div>
         </div>
-
         <div class="feature col-md-4">
             <h2>Informatique</h2>
             <p>
@@ -57,11 +74,9 @@
                     </li>
                 </ul>
             </p>
-            <a href="#" class="icon-link" data-bs-toggle="modal" data-bs-target="#tdInformatique2">
-                S'exercer!!
-            </a>
-
-
+            <button href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tdInformatique2">
+                Disponible
+            </button>
             <!-- Modal -->
             <div class="modal fade" id="tdInformatique2" data-bs-backdrop="static" data-bs-keyboard="false"
                 tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -77,29 +92,81 @@
                                     Algorithmique et programmation 2
                                     <ul>
                                         <li>
-                                            <a
-                                                href="https://drive.google.com/file/d/1nJ5ZGzY_-KgYAcLn_rjcxG1K4HMp_sqA/view?usp=sharing">Iterations</a>
+                                            <ol>
+                                                <li>
+                                                    <a
+                                                        href="https://drive.google.com/file/d/1nJ5ZGzY_-KgYAcLn_rjcxG1K4HMp_sqA/view?usp=sharing">Iterations</a>
+
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        href="https://github.com/cheikh0101/correctionAlgo1/tree/main/fiche5">Proposition
+                                                        de Correction</a>
+                                                </li>
+                                            </ol>
                                         </li>
+                                        <hr>
                                         <li>
-                                            <a
-                                                href="https://drive.google.com/file/d/1v4mlmA-NLf_YmQ7nTm_JD1oJ20xCAb6i/view?usp=sharing">Types
-                                                Composés</a>
+                                            <ol>
+                                                <li>
+                                                    <a
+                                                        href="https://drive.google.com/file/d/1v4mlmA-NLf_YmQ7nTm_JD1oJ20xCAb6i/view?usp=sharing">Types
+                                                        Composés</a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        href="https://github.com/cheikh0101/correctionAlgo1/tree/main/projet-types-composes">Proposition
+                                                        de Correction</a>
+                                                </li>
+                                            </ol>
+
                                         </li>
+                                        <hr>
                                         <li>
-                                            <a
-                                                href="https://drive.google.com/file/d/1WgDnN9I-OQMI0VcHcvsLjZJ7SWu8ynzw/view?usp=sharing">Chaines
-                                                de Caractère</a>
+                                            <ol>
+                                                <li>
+                                                    <a
+                                                        href="https://drive.google.com/file/d/1WgDnN9I-OQMI0VcHcvsLjZJ7SWu8ynzw/view?usp=sharing">Chaines
+                                                        de Caractère</a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        href="https://github.com/cheikh0101/correctionAlgo1/tree/main/caractere">Proposition
+                                                        de Correction</a>
+                                                </li>
+                                            </ol>
                                         </li>
+                                        <hr>
                                         <li>
-                                            <a
-                                                href="https://drive.google.com/file/d/11K3JOUGUoLZFUGqeyuzqDBpPWkl5oORw/view?usp=sharing">Pointeurs
-                                                Preprocesseurs</a>
+                                            <ol>
+                                                <li>
+                                                    <a
+                                                        href="https://drive.google.com/file/d/11K3JOUGUoLZFUGqeyuzqDBpPWkl5oORw/view?usp=sharing">Pointeurs
+                                                        Preprocesseurs</a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        href="https://github.com/cheikh0101/correctionAlgo1/tree/main/projet-pointeur">Proposition
+                                                        de Correction</a>
+                                                </li>
+                                            </ol>
                                         </li>
+                                        <hr>
                                         <li>
-                                            <a
-                                                href="https://drive.google.com/file/d/1eXlEnqF8yPNAevTNavyHIhunSVACd7zW/view?usp=sharing">Sous
-                                                Programmes</a>
+                                            <ol>
+                                                <li>
+                                                    <a
+                                                        href="https://drive.google.com/file/d/1eXlEnqF8yPNAevTNavyHIhunSVACd7zW/view?usp=sharing">Sous
+                                                        Programmes</a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        href="https://github.com/cheikh0101/correctionAlgo1/tree/main/sous-prog">Proposition
+                                                        de Correction</a>
+                                                </li>
+                                            </ol>
                                         </li>
+                                        <hr>
                                         <li>
                                             <a
                                                 href="https://drive.google.com/file/d/1c1OqOUTmQ-TcuEcpb12GmPq7OA3KcnqC/view?usp=sharing">Examen
@@ -111,7 +178,29 @@
                                     Architecture des Ordinateurs
                                     <ul>
                                         <li>
-                                            <a href=""></a>
+                                            <a
+                                                href="https://drive.google.com/file/d/1wam1C11-xyK8chlFpjskZKVnSwWqZy5y/view?usp=sharing">Recueil
+                                                d'Exercices</a>
+                                        </li>
+                                        <li>
+                                            <a
+                                                href="https://drive.google.com/file/d/1G7nAZwSLSD1_ztQoDIF9_BP2lt8sUDJG/view?usp=sharing">Examen
+                                                novembre 2018</a>
+                                        </li>
+                                        <li>
+                                            <a
+                                                href="https://drive.google.com/file/d/1CHjPBmNsclcnaUgY_I-YQV69vOpWOyi0/view?usp=sharing">Devoir
+                                                1</a>
+                                        </li>
+                                        <li>
+                                            <a
+                                                href="https://drive.google.com/file/d/116Va_yzrYueCgf-OG70fLPBdMrmSiWFS/view?usp=sharing">Devoir
+                                                2</a>
+                                        </li>
+                                        <li>
+                                            <a
+                                                href="https://drive.google.com/file/d/1AHJQQd2NgIq2Tvj73S3MWog0cn_xmmer/view?usp=sharing">Serie
+                                                d'exercices</a>
                                         </li>
                                     </ul>
                                 </li>
