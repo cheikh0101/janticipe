774ab3704cf70026b37d4e66d445c728bb02dfa7
cheikh0101 <ctidiane.gueye1@univ-thies.sn>
2022-06-15 21:57:27 +0000

    Suppression de la banniere de bonne rentree universitaire

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -14,7 +14,7 @@
 </head>
 
 <body>
-    
+
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
@@ -65,12 +65,6 @@
     <header class="bg-primary py-5 mb-5">
         <div class="container h-100">
             <div class="row h-100 align-items-center">
-                <div class="col-lg-12 mt-2">
-                    <div class="alert alert-primary alert-dismissible fade show" role="alert">
-                        {{ config('app.name') }} souhaite une bonne rentrée universitaire à tous les nouveaux bacheliers 
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                </div>
                 <div class="col-lg-12">
                     <h1 class="display-4 text-white mt-5 mb-2">{{ config('app.name') }}</h1>
                     <p class="lead mb-5 text-white">
